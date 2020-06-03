b49c2f3697cf19ce365864821f4ed1c8880fb09f
liujianpost <liujianpost@163.com>
2020-06-03 11:28:25 +0800

    jpa project

---
+++ jpa/jpa-persist/src/main/java/Controller.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootConfiguration
+public class Controller {
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+}
